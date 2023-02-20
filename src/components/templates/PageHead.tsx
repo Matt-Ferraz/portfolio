4c7ced03b8adfce59ccc9db1186b8dca91ae2a47
Matt-Ferraz <helinhorx2016@gmail.com>
2023-02-19 22:42:14 -0300

    Added atomic code organization

---
+++ src/components/templates/PageHead.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+interface HeadProps {
+	title: string,
+	favIconPath: string
+};
+
+export default function PageHead(props: HeadProps) {
+	return (
+		<Head>
+			<title>{ props.title }</title>
+			<link rel="icon" href={ props.favIconPath }/>
+		</Head>
+	)
+};
